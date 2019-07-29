844a0bc0b52cff73e09ad54bc5f833560b436dd5
Daniel <danielrasheed@outlook.com>
2019-07-29 18:51:59 +0200

    changing port

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ if (module.hot) {
   console.info('✅  Server-side HMR Enabled!');
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 export default express()
   .use((req, res) => app.handle(req, res))
